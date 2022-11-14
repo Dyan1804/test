c3f88ef25a30f5f7102381c0883214bcb14d35ea
Dyan1804 <duyanhocmnd@gmail.com>
2022-11-14 21:56:32 +0700

    Duy Anh da sua file.

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
 	<div class="container">
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h2 class="text-center">Welcome.</h2>
+				<h2 class="text-center">Xin chao cac ban.</h2>
 			</div>
 			<div class="panel-body">
 				<form method="post" action="index.php">
